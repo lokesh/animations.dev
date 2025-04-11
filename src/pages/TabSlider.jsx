90538413156e4732a12bae41b79ddf3d0345e697
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2025-04-11 11:42:59 -0700

    Add react-icons dependency and update TabSlider component: integrate icons for tabs, enhance styling, and remove unused react.svg asset.

--- src/pages/TabSlider.jsx
+++ src/pages/TabSlider.jsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
 import styles from '../styles/TabSlider.module.css';
+import { LuCircleCheck, LuUtensilsCrossed, LuBrush, LuSettings } from 'react-icons/lu';
 
 const TabSlider = () => {
   const [activeTab, setActiveTab] = useState(0);
 
   const tabs = [
-    { id: 0, label: 'Tab 1', content: 'Content for Tab 1' },
-    { id: 1, label: 'Tab 2', content: 'Content for Tab 2' },
-    { id: 2, label: 'Tab 3', content: 'Content for Tab 3' },
+    { id: 0, label: 'Habits', content: 'Content for Tab 1', icon: <LuCircleCheck /> },
+    { id: 1, label: 'Meals', content: 'Content for Tab 2', icon: <LuUtensilsCrossed /> },
+    { id: 2, label: 'Chores', content: 'Content for Tab 3', icon: <LuBrush /> },
+    { id: 3, label: 'Settings', content: 'Content for Tab 4', icon: <LuSettings /> },
   ];
+
 
   return (
     <div className={styles.container}>
-      <h1>Tab Slider</h1>
+      <div 
+        className={`${styles.tabContainer} ${styles.tabContainerActive} ${styles[`tab${activeTab}`]}`}
+      >
+        {tabs.map((tab) => (
+          <button
+            key={tab.id}
+            className={`${styles.tab} ${activeTab === tab.id ? styles.active : ''}`}
+            onClick={() => setActiveTab(tab.id)}
+            tabIndex="-1"
+          >
+            <span className={styles.icon} tabIndex="-1">{tab.icon}</span>
+            {tab.label}
+          </button>
+        ))}
+      </div>
       <div className={styles.tabContainer}>
         {tabs.map((tab) => (
           <button
@@ -20,6 +37,7 @@ const TabSlider = () => {
             className={`${styles.tab} ${activeTab === tab.id ? styles.active : ''}`}
             onClick={() => setActiveTab(tab.id)}
           >
+            <span className={styles.icon} tabIndex="-1">{tab.icon}</span>
             {tab.label}
           </button>
         ))}
